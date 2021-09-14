3e40b1092e5f4f511bf1d9dd9309e166d624e873
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-14 15:49:23 +0700

    Variable - Vanilla Javascript

--- var.js
+++ var.js
@@ -1,3 +1,12 @@
-var iceCream = "Coklat";
+let iceCream = "Coklat";
 
 console.log(iceCream);
+
+iceCream = "Strawberry";
+
+console.log(iceCream);
+
+/*
+ variable yang bernilai const (constanta) isi nya tidak dapat berubah pada baris selanjutnya jika dibaris selanjutnya
+ ada kondisi untuk merubah sebuah variable. lain cerita jika menggunakan variable yang bernilai selain constanta. ex; let
+*/ 
