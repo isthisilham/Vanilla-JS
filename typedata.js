5133c941712059b1d279c9b9f2c37f435061887c
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-14 15:59:50 +0700

    Type of Data - Vanilla Javascript

---
+++ typedata.js
@@ -0,0 +1,15 @@
+const plate = undefined;
+
+console.log(typeof plate);
+
+let bowl = 1;
+
+console.log(typeof bowl);
+
+bowl = "red";
+
+console.log(typeof bowl);
+
+bowl = true;
+
+console.log(typeof bowl);
