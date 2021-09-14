bffc379b7899b61ec389a5d75427806668d8218a
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-14 16:51:36 +0700

    Operator - Vanilla Javascript

---
+++ operator.js
@@ -0,0 +1,27 @@
+const assigment = 5 + 1;
+
+console.log(assigment);
+
+let comparison = 5 >= 2;
+
+if (comparison == true) console.log("isTrue");
+else console.log("isFalse")
+
+comparison = 10 % 4;
+
+if (comparison > 1) console.log(comparison);
+else console.log("Nilai lebih besar dari 1 ")
+
+let logical1 = "saya suka kamu"
+let logical2 = "dan kamu suka aku"
+
+if (logical1 && logical2) console.log("<3")
+
+let increment = "5"
+
+console.log(++increment)
+
+let decrement = "10"
+
+console.log(--decrement)
+
