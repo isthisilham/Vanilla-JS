266079e9e1660a38e7218f10bad2ea1223f7b615
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-17 11:16:38 +0700

    Object Access - Vanilla Javascript

--- object.js
+++ object.js
@@ -9,9 +9,21 @@ const Person = {
   isWeightIdeal: function () {
     const heightMinus100 = this.height - 100;
 
-    return (heightMinus100 - (heightMinus100 * 10) / 100) < this.weight ;
+    return heightMinus100 - (heightMinus100 * 10) / 100 < this.weight;
   },
 };
 
 console.log(Person.fullName());
 console.log(Person.isWeightIdeal());
+
+const university = {
+  UI: {
+    Fakultas: ["Ilmu Bahasa", "Ilmu Budaya", "Teknik"],
+    Teknik: ["Informatika", "Mesin", "Kapal"],
+  },
+  ITB: {},
+
+  UGM: {},
+};
+
+console.log(university?.UI?.Teknik ?? "Tidak Ditemukan");
