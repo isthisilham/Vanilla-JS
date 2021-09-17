c8e1778505f0d181d0952a1980b9ac23713a92c1
Muhammad Ilham <muhammadilham6656@gmail.com>
2021-09-17 11:01:12 +0700

    Object - Vanilla Javascript

--- object.js
+++ object.js
@@ -4,6 +4,14 @@ const Person = {
   fullName: function () {
     return `${this.firstName} ${this.lastName}`;
   },
+  weight: 67,
+  height: 175,
+  isWeightIdeal: function () {
+    const heightMinus100 = this.height - 100;
+
+    return (heightMinus100 - (heightMinus100 * 10) / 100) < this.weight ;
+  },
 };
 
-console.log(Person.fullName())
+console.log(Person.fullName());
+console.log(Person.isWeightIdeal());
